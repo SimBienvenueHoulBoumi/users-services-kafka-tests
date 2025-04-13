329cab1279f1395be99b286a33450c237fcfc473
sim <brhulla@gmail.com>
2025-04-13 12:46:02 +0200

    initialisation

---
+++ src/test/java/primerriva/users_services/UsersServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package primerriva.users_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
